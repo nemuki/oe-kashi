da5452ba05a142f8fcd0ff03df8e6d6afadabaf8
Naoki Takahashi <nemuki@users.noreply.github.com>
2023-04-14 22:00:03 +0900

    `npx prettier --write .`

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,37 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "prettier"
-    ],
-    "overrides": [],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
+    plugins: ['react'],
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off',
+    },
 }
---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: false,
+    singleQuote: true,
+    tabWidth: 4,
+    useTabs: false,
+    printWidth: 120,
+    proseWrap: 'preserve',
+    trailingComma: 'all',
+}
