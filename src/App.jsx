340e5a8b00acff6daf9c08a9104c91747246f536
Naoki Takahashi <nemuki@users.noreply.github.com>
2023-04-15 22:02:36 +0900

    player の位置を変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,73 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import { Player } from 'textalive-app-api'
 import TextAliveHeader from './components/header.jsx'
 
-function App() {
-    // TextAlive Player
-    const player = new Player({
-        // オプション一覧
-        // https://developer.textalive.jp/packages/textalive-app-api/interfaces/playeroptions.html
-        app: { token: import.meta.env.VITE_TEXT_ALIVE_APP_TOKEN },
-        mediaElement: document.querySelector('#media'),
-        mediaBannerPosition: 'bottom right',
-    })
+// TextAlive Player
+const player = new Player({
+    // オプション一覧
+    // https://developer.textalive.jp/packages/textalive-app-api/interfaces/playeroptions.html
+    app: { token: import.meta.env.VITE_TEXT_ALIVE_APP_TOKEN },
+    mediaElement: document.querySelector('#media'),
+    mediaBannerPosition: 'bottom right',
+})
 
+function App() {
     const [artist, setArtist] = useState('')
     const [songName, setSongName] = useState('')
+    const [playOrPause, setPlayOrPause] = useState('再生')
     const [onTimerReady, setOnTimerReady] = useState(false)
 
-    useEffect(() => {
-        player.addListener({
-            /* APIの準備ができたら呼ばれる */
-            onAppReady(app) {
-                if (app.managed) {
-                    document.querySelector('#control').className = 'disabled'
-                }
-                if (!app.songUrl) {
-                    document.querySelector('#media').className = 'disabled'
+    player.addListener({
+        /* APIの準備ができたら呼ばれる */
+        onAppReady(app) {
+            if (app.managed) {
+                document.querySelector('#control').className = 'disabled'
+            }
+            if (!app.songUrl) {
+                document.querySelector('#media').className = 'disabled'
 
-                    // https://developer.textalive.jp/events/magicalmirai2023/
-                    // king妃jack躍 / 宮守文学 feat. 初音ミク
-                    player.createFromSongUrl('https://piapro.jp/t/ucgN/20230110005414', {
-                        video: {
-                            // 音楽地図訂正履歴: https://songle.jp/songs/2427948/history
-                            beatId: 4267297,
-                            chordId: 2405019,
-                            repetitiveSegmentId: 2405019,
-                            // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FucgN%2F20230110005414
-                            lyricId: 56092,
-                            lyricDiffId: 9636,
-                        },
-                    })
-                }
-            },
+                // https://developer.textalive.jp/events/magicalmirai2023/
+                // king妃jack躍 / 宮守文学 feat. 初音ミク
+                player.createFromSongUrl('https://piapro.jp/t/ucgN/20230110005414', {
+                    video: {
+                        // 音楽地図訂正履歴: https://songle.jp/songs/2427948/history
+                        beatId: 4267297,
+                        chordId: 2405019,
+                        repetitiveSegmentId: 2405019,
+                        // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FucgN%2F20230110005414
+                        lyricId: 56092,
+                        lyricDiffId: 9636,
+                    },
+                })
+            }
+        },
 
-            /* 楽曲情報が取れたら呼ばれる */
-            onVideoReady() {
-                // 楽曲情報を表示
-                setArtist(player.data.song.artist.name)
-                setSongName(player.data.song.name)
-            },
+        /* 楽曲情報が取れたら呼ばれる */
+        onVideoReady() {
+            // 楽曲情報を表示
+            setArtist(player.data.song.artist.name)
+            setSongName(player.data.song.name)
+        },
 
-            /* 再生コントロールができるようになったら呼ばれる */
-            onTimerReady() {
-                setOnTimerReady(true)
-            },
+        /* 再生コントロールができるようになったら呼ばれる */
+        onTimerReady() {
+            setOnTimerReady(true)
+        },
 
-            /* 楽曲の再生が始まったら呼ばれる */
-            onPlay() {
-                const a = document.querySelector('#control > a#play')
-                while (a.firstChild) a.removeChild(a.firstChild)
-                a.appendChild(document.createTextNode('\uf28b'))
-            },
+        /* 楽曲の再生が始まったら呼ばれる */
+        onPlay() {
+            setPlayOrPause('停止')
+        },
 
-            /* 楽曲の再生が止まったら呼ばれる */
-            onPause() {
-                const a = document.querySelector('#control > a#play')
-                while (a.firstChild) a.removeChild(a.firstChild)
-                a.appendChild(document.createTextNode('\uf144'))
-            },
-        })
-    }, [])
+        /* 楽曲の再生が止まったら呼ばれる */
+        onPause() {
+            setPlayOrPause('再生')
+        },
+    })
 
-    const onPlay = () => {
+    const playMusic = () => {
+        console.log('つらい')
         if (player) {
             if (player.isPlaying) {
                 player.requestPause()
@@ -83,7 +79,13 @@ function App() {
 
     return (
         <div className="App">
-            <TextAliveHeader artist={artist} songName={songName} onTimerReady={onTimerReady} onPlay={() => onPlay()} />
+            <TextAliveHeader
+                artist={artist}
+                songName={songName}
+                onTimerReady={onTimerReady}
+                playOrPause={playOrPause}
+                playMusic={playMusic}
+            />
             <div id="media"></div>
         </div>
     )
