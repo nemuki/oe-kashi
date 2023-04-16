db7a5ab637184415be7a1ec9152f56ee1b893a26
Naoki Takahashi <nemuki@users.noreply.github.com>
2023-04-16 22:50:37 +0900

    useEffect に入れる

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { Player } from 'textalive-app-api'
 import TextAliveHeader from './components/header.jsx'
@@ -18,53 +18,55 @@ function App() {
     const [playOrPause, setPlayOrPause] = useState('再生')
     const [onTimerReady, setOnTimerReady] = useState(false)
 
-    player.addListener({
-        /* APIの準備ができたら呼ばれる */
-        onAppReady(app) {
-            if (app.managed) {
-                document.querySelector('#control').className = 'disabled'
-            }
-            if (!app.songUrl) {
-                document.querySelector('#media').className = 'disabled'
+    useEffect(() => {
+        player.addListener({
+            /* APIの準備ができたら呼ばれる */
+            onAppReady(app) {
+                if (app.managed) {
+                    document.querySelector('#control').className = 'disabled'
+                }
+                if (!app.songUrl) {
+                    document.querySelector('#media').className = 'disabled'
 
-                // https://developer.textalive.jp/events/magicalmirai2023/
-                // king妃jack躍 / 宮守文学 feat. 初音ミク
-                player.createFromSongUrl('https://piapro.jp/t/ucgN/20230110005414', {
-                    video: {
-                        // 音楽地図訂正履歴: https://songle.jp/songs/2427948/history
-                        beatId: 4267297,
-                        chordId: 2405019,
-                        repetitiveSegmentId: 2405019,
-                        // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FucgN%2F20230110005414
-                        lyricId: 56092,
-                        lyricDiffId: 9636,
-                    },
-                })
-            }
-        },
+                    // https://developer.textalive.jp/events/magicalmirai2023/
+                    // king妃jack躍 / 宮守文学 feat. 初音ミク
+                    player.createFromSongUrl('https://piapro.jp/t/ucgN/20230110005414', {
+                        video: {
+                            // 音楽地図訂正履歴: https://songle.jp/songs/2427948/history
+                            beatId: 4267297,
+                            chordId: 2405019,
+                            repetitiveSegmentId: 2405019,
+                            // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FucgN%2F20230110005414
+                            lyricId: 56092,
+                            lyricDiffId: 9636,
+                        },
+                    })
+                }
+            },
 
-        /* 楽曲情報が取れたら呼ばれる */
-        onVideoReady() {
-            // 楽曲情報を表示
-            setArtist(player.data.song.artist.name)
-            setSongName(player.data.song.name)
-        },
+            /* 楽曲情報が取れたら呼ばれる */
+            onVideoReady() {
+                // 楽曲情報を表示
+                setArtist(player.data.song.artist.name)
+                setSongName(player.data.song.name)
+            },
 
-        /* 再生コントロールができるようになったら呼ばれる */
-        onTimerReady() {
-            setOnTimerReady(true)
-        },
+            /* 再生コントロールができるようになったら呼ばれる */
+            onTimerReady() {
+                setOnTimerReady(true)
+            },
 
-        /* 楽曲の再生が始まったら呼ばれる */
-        onPlay() {
-            setPlayOrPause('停止')
-        },
+            /* 楽曲の再生が始まったら呼ばれる */
+            onPlay() {
+                setPlayOrPause('停止')
+            },
 
-        /* 楽曲の再生が止まったら呼ばれる */
-        onPause() {
-            setPlayOrPause('再生')
-        },
-    })
+            /* 楽曲の再生が止まったら呼ばれる */
+            onPause() {
+                setPlayOrPause('再生')
+            },
+        })
+    }, [])
 
     const playMusic = () => {
         if (player) {
