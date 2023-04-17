5fa47658c77b867ed7b3c8ad678dfa3b36451845
Naoki Takahashi <nemuki@users.noreply.github.com>
2023-04-17 23:12:18 +0900

    簡単に曲を変えられるように

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import { Player } from 'textalive-app-api'
 import TextAliveController from './components/TextAliveController.jsx'
 import Mouse from './components/Mouse.jsx'
+import { contestSongs } from './ContestSongsConstraint.js'
 
 function App() {
     const [app, setApp] = useState(null)
     const [player, setPlayer] = useState(null)
     const [mediaElement, setMediaElement] = useState(null)
 
+    const [song, setSong] = useState(contestSongs[1])
     const [artistName, setArtistName] = useState('')
     const [songName, setSongName] = useState('')
     const [lyrics, setLyrics] = useState([''])
@@ -39,16 +41,8 @@ function App() {
                 if (!app.songUrl) {
                     // https://developer.textalive.jp/events/magicalmirai2023/
                     // king妃jack躍 / 宮守文学 feat. 初音ミク
-                    player.createFromSongUrl('https://piapro.jp/t/ucgN/20230110005414', {
-                        video: {
-                            // 音楽地図訂正履歴: https://songle.jp/songs/2427948/history
-                            beatId: 4267297,
-                            chordId: 2405019,
-                            repetitiveSegmentId: 2405019,
-                            // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FucgN%2F20230110005414
-                            lyricId: 56092,
-                            lyricDiffId: 9636,
-                        },
+                    player.createFromSongUrl(song.url, {
+                        video: song.video,
                     })
                 }
                 setApp(app)
