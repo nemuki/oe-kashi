728bc478e995705c3a1e7fb8ff765e04da347bf5
Naoki Takahashi <nemuki@users.noreply.github.com>
2023-04-19 00:09:18 +0900

    onpointermove

--- src/components/Mouse.jsx
+++ src/components/Mouse.jsx
@@ -4,7 +4,7 @@ function Mouse() {
     const [mouseCoordinates, setMouseCoordinates] = useState({ x: 0, y: 0 })
 
     useEffect(() => {
-        onmousemove = (event) => {
+        onpointermove = (event) => {
             setMouseCoordinates({ x: event.clientX, y: event.clientY })
         }
     }, [mouseCoordinates])
