01c57d3bc8e896c03db96c28608a9ec29e2408e1
Naoki Takahashi <nemuki@users.noreply.github.com>
2023-05-28 19:31:33 +0900

    react-device-detect

--- src/components/Mouse.jsx
+++ src/components/Mouse.jsx
@@ -1,11 +1,22 @@
 import { useEffect, useState } from 'react'
+import { isMobile } from 'react-device-detect'
 
 function Mouse() {
   const [mouseCoordinates, setMouseCoordinates] = useState({ x: 0, y: 0 })
 
   useEffect(() => {
-    onpointermove = (event) => {
-      setMouseCoordinates({ x: event.clientX, y: event.clientY })
+    if (isMobile) {
+      ontouchmove = (event) => {
+        event.preventDefault()
+        const touch = event.changedTouches
+        for (let i = 0; i < touch.length; i++) {
+          setMouseCoordinates({ x: touch[i].pageX, y: touch[i].pageY })
+        }
+      }
+    } else {
+      onpointermove = (event) => {
+        setMouseCoordinates({ x: event.clientX, y: event.clientY })
+      }
     }
   }, [mouseCoordinates])
 
