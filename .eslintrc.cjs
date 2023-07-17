b1da0c61622c1b1feef1008cfd694499612f1b2e
Naoki Takahashi <nemuki@users.noreply.github.com>
2023-07-18 00:40:22 +0900

    no-undef なくす

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,9 @@
-// eslint-disable-next-line no-undef
 module.exports = {
   root: true,
   env: {
     browser: true,
     es2022: true,
+    node: true,
   },
   extends: [
     'eslint:recommended',
