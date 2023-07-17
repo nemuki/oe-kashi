984fd1ac1ed10404c7a6e869b537a819192b8c30
Naoki Takahashi <nemuki@users.noreply.github.com>
2023-07-17 12:17:09 +0900

    global theme 入れる

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/index.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      html: {
+        h: '100%',
+        overflow: 'hidden',
+      },
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
